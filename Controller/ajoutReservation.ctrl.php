6f90963a937353b3328bd7c57dc10c2d63f0926f
Alex <paperalex59100@hotmail.fr>
2023-07-03 16:06:00 +0200

    Reservation ended, ReservationController started

---
+++ Controller/ajoutReservation.ctrl.php
@@ -0,0 +1,8 @@
+<?php
+    $arrivee = $_POST["arrivee"];
+    $depart = $_POST["depart"];
+    $nbPersonne = $_POST["nbPersonne"];
+    $classe = $_POST["classe"];
+    $emplacement = $_POST["emplacement"];
+
+    
--- View/reservation.php
+++ View/reservation.php
@@ -8,16 +8,39 @@
 <body>
     <!-- Récupération des valeurs indiquées sur la page précédente -->
     <?php
-    $arrivee = $_POST["arrivee"];
-    $depart = $_POST["depart"];
-    $nbPersonne = $_POST["nbPersonne"];
-    $classe = $_POST["classe"];
+        $arrivee = $_POST["arrivee"];
+        $depart = $_POST["depart"];
 
-    if ($arrivee > $depart) {
-        header("Location: index.php?error=Les dates sélectionnées sont invalides");
-    }
+        // Condition vérifiant que les dates entrées précédemment sont valides
+        if ($arrivee > $depart) {
+            header("Location: index.php?error=Les dates sélectionnées sont invalides");
+        }
     ?>
 
-    <p>Entrez vos informations bancaires</p>
+    <!-- Formulaire d'informations supplémentaire afin de finaliser la réservation -->
+    <form action="../Controller/ajoutReservation.ctrl.php" method="post">
+        <label for="emplacement">Emplacement</label>
+        <select name="emplacement" id="emplacement" required>
+            <option value="default">--Choisissez votre Emplacement</option>
+            <option value="north">Vue nord</option>
+            <option value="south">Vue sud</option>
+            <option value="west">Vue ouest</option>
+            <option value="east">Vue est</option>
+        </select>
+        <p>Entrez vos informations bancaires</p>
+        <label for="cbNumber">Numéro de carte bancaire</label>
+        <input type="number" id="cbNumber" name="cbNumber" required/>
+
+        <label for="expiration">Date d'expiration</label>
+        <input type="month" id="expiration" name="expiration" required />
+
+        <label for="cv">Cryptogramme visuel</label>
+        <input type="number" id="cv" name="cv" required/>
+
+        <input type="submit" id="valider" name="valider">
+    </form>
+
+    
+
 </body>
 </html>
